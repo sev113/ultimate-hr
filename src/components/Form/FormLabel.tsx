4edcb6ce2ddcaf0d8df09937d08f00ed925c3f5d
Severus <nakedsev113@gmail.com>
2023-07-20 14:57:09 +0630

    created FormLabel component

---
+++ src/components/Form/FormLabel.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { TextProps } from "react-native";
+import { Text } from "components/Text";
+
+interface FormLabelProps extends TextProps {}
+
+export default function FormLabel(props: FormLabelProps) {
+  const { children, style = {}, ...textProps } = props;
+  return (
+    <Text {...textProps} style={{ fontSize: 12, ...(style as Object) }}>
+      {children}
+    </Text>
+  );
+}
