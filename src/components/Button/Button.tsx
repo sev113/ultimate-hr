1dfd110511cb8dbf53110f34306f7d1537da55c2
Severus <nakedsev113@gmail.com>
2023-07-20 13:58:08 +0630

    added button component

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,61 @@
+import React from "react";
+import {
+  Pressable,
+  StyleProp,
+  ViewStyle,
+  TextStyle,
+  PressableProps,
+} from "react-native";
+import { Text } from "components/Text";
+
+export interface ButtonProps extends PressableProps {
+  text?: string;
+  textStyle?: StyleProp<TextStyle>;
+  disabledBgColor?: string;
+}
+
+const Button: React.FC<React.PropsWithChildren<ButtonProps>> = (props) => {
+  const {
+    text,
+    textStyle,
+    disabledBgColor,
+    style,
+    children,
+    ...pressableProps
+  } = props;
+  const customStyle = style as ViewStyle;
+  return (
+    <Pressable
+      style={({ pressed }) => [
+        {
+          justifyContent: "center",
+          padding: 10,
+          borderRadius: 5,
+          alignItems: "center",
+          opacity: pressed ? 0.5 : 1,
+          ...customStyle,
+          backgroundColor: pressableProps.disabled
+            ? disabledBgColor
+              ? disabledBgColor
+              : "rgba(0,0,0,0.2)"
+            : pressed
+            ? customStyle?.backgroundColor
+              ? customStyle.backgroundColor
+              : "rgba(0,0,0,0.1)"
+            : customStyle.backgroundColor || "#fff",
+        },
+      ]}
+      {...pressableProps}
+    >
+      {children ? (
+        children
+      ) : (
+        <Text style={{ color: "#000", ...(textStyle as TextStyle) }}>
+          {text}
+        </Text>
+      )}
+    </Pressable>
+  );
+};
+
+export default Button;
