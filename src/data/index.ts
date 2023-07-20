4a5af29a98f8f588a26b63ac82d293c0eac6e01e
Severus <nakedsev113@gmail.com>
2023-07-20 11:14:14 +0630

    added data

---
+++ src/data/index.ts
@@ -0,0 +1,57 @@
+export const GENDER = ["male", "female", "others"];
+
+export const MARITAL_STATUS = ["single", "married"];
+
+export const JOB_TYPE = ["internship", "probation", "permanent"];
+
+export const CITY = [
+  "yangon",
+  "mandalay",
+  "myit kyi na",
+  "taunggyi",
+  "nay pyi taw",
+  "magwe",
+  "taungoo",
+];
+
+export const TOWNSHIP = [
+  "hlaing",
+  "lanmadaw",
+  "latha",
+  "botahtaung",
+  "mayangone",
+];
+
+export const NRC_DIVISION = [
+  "1/",
+  "2/",
+  "3/",
+  "4/",
+  "5/",
+  "6/",
+  "7/",
+  "8/",
+  "9/",
+  "10/",
+  "11/",
+  "12/",
+  "13/",
+  "14/",
+];
+
+export const POSITION = [
+  "Web Developer",
+  "Mobile Developer",
+  "Backend Developer",
+  "UI/UX Desinger",
+  "Software Tester",
+];
+
+export const DEPARTMENT = [
+  "Information Technology",
+  "Human Resource",
+  "Academic",
+  "Marketing",
+];
+
+export const BRANCH = ["hlaing", "downtown"];
