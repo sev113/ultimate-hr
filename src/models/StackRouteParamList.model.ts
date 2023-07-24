eefa97a804377d9bce8496dfe91651bf5cf07841
Severus <nakedsev113@gmail.com>
2023-07-24 13:04:45 +0630

    Add StackRouteParamList

---
+++ src/models/StackRouteParamList.model.ts
@@ -0,0 +1,8 @@
+type IStackRouteParamList = {
+  MainScreen: undefined;
+  LoginScreen: undefined;
+  RegisterScreen: undefined;
+  HomeScreen: undefined | { data: any };
+};
+
+export default IStackRouteParamList;
